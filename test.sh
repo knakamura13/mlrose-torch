95ce75632c562f23b4b8c95f6c6e14069d815e92
Kyle Nakamura <knakamura13dev@gmail.com>
2024-07-22 00:18:54 -0700

    Move test.sh to root dir for ease of use

--- test.sh
+++ test.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-ROOT_PATH=$(pwd)/..
-export PYTHONPATH=$ROOT_PATH
-
 echo "Running tests on test_activation.py"
-python test_activation.py
+python tests/test_activation.py
 
 echo "Running tests on test_decay.py"
-python test_decay.py
+python tests/test_decay.py
 
 echo "Running tests on test_fitness.py"
-python test_fitness.py
+python tests/test_fitness.py
 
 echo "Running tests on test_algorithms.py"
-python test_algorithms.py
+python tests/test_algorithms.py
 
 echo "Running tests on test_opt_probs.py"
-python test_opt_probs.py
+python tests/test_opt_probs.py
 
 echo "Running tests on test_neural.py"
-python test_neural.py
+python tests/test_neural.py
 
 echo "Finished all tests"
